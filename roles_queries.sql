ae1816d44ac6868b0425f47ec7fa88a749f0aa98
Nirmal Patel <120539945+Nirmal7086@users.noreply.github.com>
2024-03-05 16:21:11 -0500

    roles_queries.sql

---
+++ roles_queries.sql
@@ -0,0 +1,36 @@
+set verify off
+accept WhichUser CHAR PROMPT "Roles for which user or role? - ";
+select 
+	granted_role, 
+	admin_option,
+	default_role
+from dba_role_privs
+where grantee = upper('&WhichUser')
+order by granted_role
+/
+
+----- showpriv.sql
+-- show all roles and privs granted to them
+set verify off
+col privtype heading 'PRIV|TYPE'
+col role format a17
+col priv heading 'PRIVILEGE'
+clear breaks
+break on role skip 2
+@title80 'Roles & SysPrivs/Roles Assigned To Them'
+select 
+	a.role, 
+	b.granted_role  priv , 
+	'ROLE' privtype
+from dba_roles a, dba_role_privs b
+where b.grantee = a.role
+	and b.granted_role <> a.role
+union
+select 
+	a.role,
+	b.privilege priv , 
+	'PRIV' privtype
+from dba_roles a, dba_sys_privs b
+where b.grantee = a.role
+order by 1,2,3
+/
